6458ce49c60c4040f87eb19299fd388fd2b99091
Revathyviswa <hemarev05@gmail.com>
2016-08-08 10:32:23 +0530

    Add files via upload

---
+++ JavaApplication3.java
@@ -0,0 +1,31 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication3;
+
+import com.sun.org.apache.xpath.internal.objects.Comparator;
+
+/**
+ *
+ * @author Student
+ */
+public class JavaApplication3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      public class LexicographicSort implements Comparator<Integer> {
+public int compare(Integer o1, Integer o2) {
+String s1 = o1.toString();
+String s2 = o2.toString();
+return (s2+s1).compareTo(s1+s2);
+}
+public static void main(String[] args) {
+LexicographicSort ls = new LexicographicSort();
+Integer[] nums = {9,1,95,17,5};
+Arrays.sort(nums, ls);
+System.out.println(Arrays.toString(nums));
+    }
+}}
